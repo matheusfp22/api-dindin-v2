03d59a4a0658beb7456668d3e5f7c044699cf75c
Matheus Piovezan <matheuspiovezan@outlook.com>
2024-05-02 16:18:38 -0300

    adding

---
+++ src/test/java/com/example/dindin/DindinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dindin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DindinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
