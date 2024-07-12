f5a6b5aa1b181e46639e90c2050ef629feb90bb4
Matheus Piovezan <matheuspiovezan@outlook.com>
2024-07-12 14:04:22 -0300

    adding: authentication

---
+++ src/main/java/com/example/dindin/controllers/AuthController.java
@@ -0,0 +1,59 @@
+package com.example.dindin.controllers;
+
+import com.example.dindin.configurations.security.TokenService;
+import com.example.dindin.dto.LoginRequestDTO;
+import com.example.dindin.dto.RegisterRequestDTO;
+import com.example.dindin.dto.ResponseDTO;
+import com.example.dindin.entity.User;
+import com.example.dindin.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginRequestDTO body) {
+        User user = this.repository.findByEmail(body.email()).orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (passwordEncoder.matches(body.password(), user.getPassword())) {
+            String token = this.tokenService.generateToken(user);
+            return ResponseEntity.ok(new ResponseDTO(user.getName(), token));
+        }
+
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody RegisterRequestDTO body) {
+        Optional<User> user = this.repository.findByEmail(body.email());
+
+        if (user.isEmpty()) {
+            User newUser = new User();
+            newUser.setPassword(passwordEncoder.encode(body.password()));
+            newUser.setEmail(body.email());
+            newUser.setName(body.name());
+            this.repository.save(newUser);
+            String token = this.tokenService.generateToken(newUser);
+            return ResponseEntity.ok(new ResponseDTO(newUser.getName(), token));
+        }
+
+        return ResponseEntity.badRequest().build();
+    }
+}
---
+++ src/main/java/com/example/dindin/dto/ResponseDTO.java
@@ -0,0 +1,4 @@
+package com.example.dindin.dto;
+
+public record ResponseDTO (String name, String token) {
+}
